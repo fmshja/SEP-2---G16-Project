91f33ff26be7a8b75b63efd64f3ac27aeaa04de8
NEKVaananen <91272404+NEKVaananen@users.noreply.github.com>
2021-10-20 01:44:28 +0300

    Fixed the bug and extended demos functions

--- interestsComponent/admin/displayInterests.php
+++ interestsComponent/admin/displayInterests.php
@@ -9,26 +9,47 @@ defined('_JEXEC') or die('Restricted access');
 //establish the database connection
 $db = JFactory::getDbo();
 $query = $db->getQuery(true);
-//form the query TODO: fix the problem with group id being replaced in results
-$query->select($db->quoteName(array('a.id','a.group_name','b.id','b.interest_name')));
+//form the query 
+$query->select($db->quoteName(['a.id','a.group_name','b.id','b.interest_name'],['gid','group_name','iid','interest_name']));
 $query->from($db->quoteName('app_interests_groups','a'));
 $query->innerjoin($db->quoteName('app_interests','b') . ' ON ' . $db->quoteName('a.id') . ' = ' . $db->quoteName('b.id_group'));
 
 // do the query
 $db->setQuery($query);
-$results = $db->loadObjectList();
-//echo the results temporarily to see that the query is succesful
-echo json_encode($results);
 
-//TODO: present the query results in HTML
-//TODO:add query results to the form
+//results in an array
+$results=$db->loadRowList();
+
+//do another query for group id's and names
+$query2 = $db->getQuery(true);
+$query2->select($db->quotename(array('c.id','c.group_name')));
+$query2->from($db->quotename('app_interests_groups','c'));
+$db->setQuery($query2);
+$groups=$db->loadRowList();
+
+//TODO: make the li elements clickable and make clicking submit actually do something
 ?>
 <!DOCTYPE html>
 <html>
 <body>
 <h1>This page lists interests</h1>
 <form action="" method="post">
-  
+    <?php
+    //loop the second query for group names
+    for($i=0;$i<count($groups);$i++){
+        $row=$groups[$i];
+        echo "<ul><b>". $row[1]. "</b>";
+        //loop the first guery for interest names and echo them where id's match
+        for($j=0;$j<count($results);$j++){
+            $row2=$results[$j];
+            if($row[0]==$row2[0]){
+                echo "<li>". $row2[3]. "</li>";
+            }
+        }
+        echo "</ul>";
+    }
+    ?>
+    <input type="submit" name="SubmitButton" value="Submit"/>
 </form>  
 
 </body>
