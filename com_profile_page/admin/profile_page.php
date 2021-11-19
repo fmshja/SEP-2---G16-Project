608bc40f671d46c61010412d9657154cd32410ee
MMetsoila <miika.metsoila@gmail.com>
2021-11-19 15:03:17 +0200

    Rename the com_user to com_profile_page Also there's a fix for properly calling the python script

---
+++ com_profile_page/admin/profile_page.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * this is admin side of component the user component. As admin does not need to 
+ * adjust their own settings here, this page houses the user pairing algorithm related matters.
+ */
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+
+$document = Factory::getDocument();
+$document->addStyleSheet("components/com_profile_page/style.css");
+
+if(isset($_POST['button'])){
+    echo "<p>it just works</p>";
+    $command = escapeshellcmd('python .\components\com_profile_page\run.py');
+    $output = shell_exec($command.' 2>&1');
+    echo "<p>Output:</p><p>$output</p>";
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Algorithm</title>
+</head>
+<body>
+<div class="Big_Button">
+    <form action="" method="post">
+        <input type="submit" name="button" value="Start the algorithm"/>
+    </form>
+</div>
+
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
