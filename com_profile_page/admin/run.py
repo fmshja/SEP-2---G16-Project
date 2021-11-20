281a91b5a065df906a78a722e534a9aade8f802a
MMetsoila <miika.metsoila@gmail.com>
2021-11-20 17:21:58 +0200

    Fix the bug in `run.py` table insertion and autoformat

--- com_profile_page/admin/run.py
+++ com_profile_page/admin/run.py
@@ -17,7 +17,7 @@ try:
         host="127.0.0.1",
         port=3306,
         database="joomla_ncc"
-        
+
     )
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: [e]")
@@ -39,20 +39,18 @@ interests_to_users = {}
 
 for (User_Id, Interest_Id) in cur2:
     print(f"user id: {User_Id}, Interest id's': {Interest_Id}")
-    interests={Interest_Id}
-    interests=interests.pop()
-    interests2=json.loads(interests)
-    id={User_Id}
-    id=id.pop()
-    id=int(id)
+    interests = {Interest_Id}
+    interests = interests.pop()
+    interests2 = json.loads(interests)
+    id = {User_Id}
+    id = id.pop()
+    id = int(id)
     print(type(id), type(interests))
-    
-    
-    cur.execute(
-    "INSERT INTO app_sort (id, interest) VALUES (?, ?)", 
-    id, interests)
 
-#for (id, interest_name) in cur:
+    cur.execute(
+        "INSERT INTO app_sort (id, interest) VALUES (?, ?)",
+        (id, interests))
+
+# for (id, interest_name) in cur:
 #   if interest_name not in interestlist:
 #      interestlist.append(interest_name)
-
