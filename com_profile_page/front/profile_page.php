4980d2a31f7de4d66258bfb0e1e6429904885c34
NEKVaananen <91272404+NEKVaananen@users.noreply.github.com>
2021-11-21 21:33:35 +0200

    Image can now be uploaded

--- com_profile_page/front/profile_page.php
+++ com_profile_page/front/profile_page.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * This is a component
+ * This is the profile page component.
+ * Create folder profile_pictures in joomla/images for profile pictures
  */
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Factory;
 $document = Factory::getDocument();
 $document->addStyleSheet("components/com_profile_page/style.css");
+
 
 //get user
 $user = JFactory::getUser();
@@ -32,21 +34,35 @@ if(isset($_POST['submitUserData'])){
     $lname = $_POST['lname'];
     $introduction = $_POST['introduction'];
     $interests=array();
+    $user = JFactory::getUser();
+    $imagelink=null;
+
+    //if user submitted a profile picture
+    if(isset($_FILES['propic'])){
+        //verify image
+        $file=$_FILES['propic'];
+        $uploadOk=verifyFile($file);
+        //upload the picture if it passes verification
+        if($uploadOk==1){
+            $userfilename=$file['name'];
+            move_uploaded_file($file['tmp_name'],"images/profile_pictures/".$userfilename);
+        }
+    }
+
     //loop through the form and push the values to an array
     foreach($_POST['interest'] as $value){
         array_push($interests, $value);
     }
     //encode the array to json-format
     $interests=json_encode($interests);
-    $user = JFactory::getUser();
 
     //save interests to database
+    /*
     $query->clear();
     $query=$db->getQuery(true);
     $columns=array('User_Id','Interest_Id');
     $values=array($db->quote($user->id), $db->quote($interests));
 
-    $query->clear();
     $query
         ->insert($db->quoteName('app_user_interests'))
         ->columns($db->quoteName($columns))
@@ -54,6 +70,34 @@ if(isset($_POST['submitUserData'])){
     $db->setQuery($query);
     $db->execute();
     header("Refresh:0");
+    */
+}
+//check the file for various things
+function verifyFile($file){
+    $uploadOk = 1;
+    $filename=$file['name'];
+    $filesize=$file['size'];
+    $filetmp=$file['tmp_name'];
+    $filetype=$file['type'];
+    $tmp=explode('.', $filename);
+    $file_ext=end($tmp);
+    $target_file="images/profile_pictures/" . basename($filename);
+
+    $extensions= array("jpeg","jpg","png");
+    //check file type
+    if(in_array($file_ext,$extensions)=== false){
+        $uploadOk=0;
+    }
+    //check filesize
+    if($filesize > 2097152) {
+        $uploadOk=0;
+    }
+    // Check if file already with same name exists
+    if (file_exists($target_file)) {
+        $uploadOk=0;
+    }
+    //return 0 if problems were encountered. else return 1.
+    return $uploadOk;
 }
 
 ?>
@@ -82,7 +126,7 @@ if(isset($_POST['submitUserData'])){
     }
     //more user data asked
     else{
-        echo "<form action=\"\" method=\"post\">
+        echo "<form action=\"\" method=\"post\" enctype=\"multipart/form-data\">
         <h1>Welcome to the connecting colleagues!</h1>
         <h2>Let us finalize your account.</h2>
         <label for=\"fname\"><b>First name:</b></label>
@@ -105,7 +149,7 @@ if(isset($_POST['submitUserData'])){
             <textarea rows=\"4\" cols=\"50\" maxlength=\"255\" name=\"introduction\"></textarea>
 
             <h2>Upload a profile pic:</h2>
-            <input type=\"file\" name=\"propic\" id=\"fileupload\">
+            <input type=\"file\" name=\"propic\">
             <button type=\"submit\" class=\"button\" name=\"submitUserData\">save</button>
         </form>";
     }
