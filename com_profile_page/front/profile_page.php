9a5b01d924b4ac79d4fbcc5ec54be1b51a563b7d
sarapaasila <79796044+sarapaasila@users.noreply.github.com>
2021-11-27 10:07:04 +0200

    Added styling to the component's front side and supplementary html for styling

--- com_profile_page/front/profile_page.php
+++ com_profile_page/front/profile_page.php
@@ -6,17 +6,17 @@
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Factory;
 $document = Factory::getDocument();
-$document->addStyleSheet("components/com_profile_page/style.css");
+$document->addStyleSheet(JURI::root(true) . '/components/com_profile_page/style.css');
 
 
-//get user
+// Get user
 $user = JFactory::getUser();
 
-//set up access to database
+// Set up access to database
 $db = JFactory::getDbo();
 $query = $db->getQuery(true);
 
-//Check if user has already existing user data
+// Check if user has already existing user data
 try{
     $query->select($db->quoteName('id'))
     ->from($db->quoteName('app_users'))
@@ -28,7 +28,7 @@ catch(Exception $e){
 
 }
 
-//check if form was submitted
+// Check if form was submitted
 if(isset($_POST['submitUserData'])){ 
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
@@ -37,26 +37,26 @@ if(isset($_POST['submitUserData'])){
     $user = JFactory::getUser();
     $imagelink=null;
 
-    //if user submitted a profile picture
+    // If user submitted a profile picture
     if(isset($_FILES['propic'])){
-        //verify image
+        // Verify image
         $file=$_FILES['propic'];
         $uploadOk=verifyFile($file);
-        //upload the picture if it passes verification
+        // Upload the picture if it passes verification
         if($uploadOk==1){
             $userfilename=$file['name'];
             move_uploaded_file($file['tmp_name'],"images/profile_pictures/".$userfilename);
         }
     }
 
-    //loop through the form and push the values to an array
+    // Loop through the form and push the values to an array
     foreach($_POST['interest'] as $value){
         array_push($interests, $value);
     }
-    //encode the array to json-format
+    // Encode the array to json-format
     $interests=json_encode($interests);
 
-    //save interests to database
+    // Save interests to database
     /*
     $query->clear();
     $query=$db->getQuery(true);
@@ -72,7 +72,8 @@ if(isset($_POST['submitUserData'])){
     header("Refresh:0");
     */
 }
-//check the file for various things
+
+// Check the file for various things
 function verifyFile($file){
     $uploadOk = 1;
     $filename=$file['name'];
@@ -84,11 +85,11 @@ function verifyFile($file){
     $target_file="images/profile_pictures/" . basename($filename);
 
     $extensions= array("jpeg","jpg","png");
-    //check file type
+    // Check file type
     if(in_array($file_ext,$extensions)=== false){
         $uploadOk=0;
     }
-    //check filesize
+    // Check filesize
     if($filesize > 2097152) {
         $uploadOk=0;
     }
@@ -96,67 +97,93 @@ function verifyFile($file){
     if (file_exists($target_file)) {
         $uploadOk=0;
     }
-    //return 0 if problems were encountered. else return 1.
+    // Return 0 if problems were encountered, otherwise return 1.
     return $uploadOk;
 }
 
 ?>
+
 <!DOCTYPE html>
 <html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Profile</title>
+    <title>My Profile | Connecting Colleagues</title>
 </head>
 <body>
     <?php  
-    //if user has finished account creation
-    if($exist!=null){
-        echo "
-        <div class=\"container\">
-            <div class=\"side\">
-                <p>picture</p>
-                <p>name</p>
-            </div>
-            <div class=\"content_area\">
-                <p>showcase area</p>
-            </div>
-        </div>";
-    }
-    //more user data asked
-    else{
-        echo "<form action=\"\" method=\"post\" enctype=\"multipart/form-data\">
-        <h1>Welcome to the connecting colleagues!</h1>
-        <h2>Let us finalize your account.</h2>
-        <label for=\"fname\"><b>First name:</b></label>
-        <input type=\"text\" placeholder=\"Enter your first name\" name=\"fname\" required>
-        <label for=\"lname\"><b>Last name:</b></label>
-        <input type=\"text\" placeholder=\"Enter your last name\" name=\"lname\" required>
-        <h2>Select some initial interests:</h2>";
-        $query->clear();
-        $query->select($db->quoteName(['id','interest_name']))
-        ->from($db->quoteName('app_interests'));
-        $db->setQuery($query);
-        $dbinterest = $db->loadRowList();
-
-        for($i=0;$i<3;$i++){
-            $row=$dbinterest[$i];
-            echo "<input type=\"checkbox\" name=\"interest[]\" value=\"". $row[0]. "\">". $row[1]. "</input>";
+        // If user has finished account creation
+        if ($exist != null) {
+            echo "
+                <div class=\"container\">
+                    <div class=\"side\">
+                        <p>picture</p>
+                        <p>name</p>
+                    </div>
+                    <div class=\"content_area\">
+                        <p>showcase area</p>
+                    </div>
+                </div>";
         }
+        // Ask the user for more data.
+        else {
+            echo "<section class=\"additional-info bg\"";
+            echo "<div class=\"mbox\">";
+                echo "<form action=\"\" method=\"post\" enctype=\"multipart/form-data\">";
 
-        echo"<h2>Write a short introduction:</h2>
-            <textarea rows=\"4\" cols=\"50\" maxlength=\"255\" name=\"introduction\"></textarea>
+                    echo "<div class=\"mbox-instructions\">";
+                        echo "<h2>Welcome to the connecting colleagues!</h2>";
+                        echo "<h3>Let us finalize your account.</h3>";
+                    echo "</div>";
 
-            <h2>Upload a profile pic:</h2>
-            <input type=\"file\" name=\"propic\">
-            <button type=\"submit\" class=\"button\" name=\"submitUserData\">save</button>
-        </form>";
-    }
+                    echo "<div class=\"form-control\">";
+                        echo "<label for=\"fname\">Your first name</label>";
+                        echo "<input type=\"text\" name=\"fname\" class=\"text-input\" required>";
+                    echo "</div>";
+
+                    echo "<div class=\"form-control\">";
+                        echo "<label for=\"lname\">Your last name</label>";
+                        echo "<input type=\"text\" name=\"lname\" class=\"text-input\" required>";
+                    echo "</div>";
+
+                    echo "<div class=\"form-control\">";
+                        echo "<h3>Select some initial interests</h3>";
+                        // Get some intrests from the database for the user to choose from.
+                        $query->clear();
+                        $query  ->select($db->quoteName(['id','interest_name']))
+                                ->from($db->quoteName('app_interests'));
+                        $db->setQuery($query);
+                        $dbinterest = $db->loadRowList();
+
+                        for ($i = 0; $i < 3; $i++) {
+                            $row = $dbinterest[$i];
+                            echo "<input type=\"checkbox\" name=\"interest[]\" value=\"". $row[0]. "\">". $row[1]. "</input>";
+                        }
+                    echo "</div>";
+
+                    echo "<div class=\"form-control\">";
+                        echo "<h3>Write a short introduction of yourself</h3>";
+                        echo "<textarea rows=\"4\" cols=\"50\" maxlength=\"255\" name=\"introduction\"></textarea>";
+                    echo "</div>";
+
+
+                    echo "<div class=\"form-control\">";
+                        echo "<h2>Upload a profile pic</h2>";
+                        echo "<input type=\"file\" name=\"propic\">";
+                    echo "</div>";
+                    
+                    echo "<div class=\"action-buttons\">";
+                        echo "<button type=\"submit\" name=\"submitUserData\" class=\"btn\"> Save </button>";
+                    echo "</div>";
+
+                echo "</form>";
+            echo "</div>";
+        }
     ?>
 
-<script type="text/javascript">
-//add javascript here
-</script>
+    <script type="text/javascript">
+    //add javascript here
+    </script>
 </body>
 </html>
