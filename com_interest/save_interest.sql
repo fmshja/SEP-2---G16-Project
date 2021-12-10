5646ce6b7e6bf823579bc2a1a7c7004dd71dbba0
NEKVaananen <91272404+NEKVaananen@users.noreply.github.com>
2021-12-10 12:51:52 +0200

    app-messages table

--- com_interest/save_interest.sql
+++ com_interest/save_interest.sql
@@ -34,3 +34,13 @@ CREATE TABLE `app_users` (
   `introduction` text DEFAULT NULL, 
   PRIMARY KEY (`id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8; 
+
+CREATE TABLE `app_messages` ( 
+  `recipient_id` int(11) NOT NULL AUTO_INCREMENT, 
+  `sender_name` varchar(255) NOT NULL, 
+  `sender_email` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `meeting_date` varchar(20),
+  `meeting_time` varchar(20),
+  PRIMARY KEY (`recipient_id`) 
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
