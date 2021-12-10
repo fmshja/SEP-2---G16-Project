a70faed55079736504886d03f1133b553145ab0e
sarapaasila <79796044+sarapaasila@users.noreply.github.com>
2021-12-10 08:52:32 +0200

    Added styling to the debug section.

--- com_profile_page/admin/profile_page.php
+++ com_profile_page/admin/profile_page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * this is admin side of component the user component. As admin does not need to 
+ * This is admin side of component the user component. As admin does not need to 
  * adjust their own settings here, this page houses the user pairing algorithm related matters.
  */
 defined('_JEXEC') or die('Restricted access');
@@ -10,13 +10,13 @@ $document = Factory::getDocument();
 $options = array("version" => "auto");
 $document->addStyleSheet(JURI::root(true) . '/administrator/components/com_profile_page/style.css');
 
-if(isset($_POST['button'])){
+if(isset($_POST['matching-button'])){
     $command = escapeshellcmd('python .\components\com_profile_page\run.py');
     $output = shell_exec($command.' 2>&1');
     $output = nl2br($output); // change the linebreaks to <br> tags
-    echo "<p class=\"console_output\">Formed groups:<br>$output<br></p>"; // debug print
+    // Debug print
+    echo '<section class="console_output">Formed groups:<br>'. $output .'</section>';
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -27,18 +27,14 @@ if(isset($_POST['button'])){
     <title>Matching Algorithm | Connecting Colleagues</title>
 </head>
 <body>
-    <div class="Big_Button bg">
+    <section class="big-button">
         <div class="guide-text">
             <h2>A manual start-up for the matching algorithm</h2>
             <p>The admin needs to manually active the matching algorithm of the users by clicking the button below.</p>
         </div>
         <form action="" method="post">
-            <input type="submit" name="button" class="btn" value="Start the algorithm"/>
+            <input type="submit" name="matching-button" class="btn" value="Start the algorithm"/>
         </form>
-    </div>
-
-    <script type="text/javascript">
-
-    </script>
+    </section>
 </body>
 </html>
