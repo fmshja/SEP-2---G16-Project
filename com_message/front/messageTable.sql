a040fee6c6237831dc5a095b06602496f1691fa0
NEKVaananen <91272404+NEKVaananen@users.noreply.github.com>
2021-12-13 10:36:14 +0200

    Delete messageTable.sql

--- com_message/front/messageTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `app_messages` ( 
-  `recipient_id` int(11) NOT NULL AUTO_INCREMENT, 
-  `sender_name` varchar(255) NOT NULL, 
-  `sender_email` varchar(255) NOT NULL,
-  `message` varchar(255) NOT NULL,
-  `meeting_date` varchar(20),
-  `meeting_time` varchar(20),
-  PRIMARY KEY (`recipient_id`) 
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8; 
