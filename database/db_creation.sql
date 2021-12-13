a8f1a59e108666c8c8c5d5a2a41eaef9048b3793
JoeRauma <91534788+JoeRauma@users.noreply.github.com>
2021-12-13 20:32:07 +0200

    Added app_calendar_notes table

--- database/db_creation.sql
+++ database/db_creation.sql
@@ -44,3 +44,14 @@ CREATE TABLE `app_messages` (
   `meeting_time` varchar(20),
   PRIMARY KEY (`recipient_id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `app_calendar_notes` (
+`Note_Id` int(11) NOT NULL AUTO_INCREMENT,
+`User_Id` int(11) NOT NULL,
+`Label` varchar(26) NOT NULL,
+`Content` varchar(255) DEFAULT NULL,
+`Date` date NOT NULL,
+`Start_time` time,
+`End_time` time,
+PRIMARY KEY (`Note_Id`)
+) ENGINE=InnoDB CHARSET=utf8;
